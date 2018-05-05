fb88dbf7dacaef7f45488b274d9777701a4e2fbd
Jose Luis Martinez <jlmartinez@capside.com>
2018-05-05 23:18:02 +0200

    Split classes into their files

--- lib/Kubectl/CLIWrapper.pm
+++ lib/Kubectl/CLIWrapper.pm
@@ -1,18 +1,8 @@
-package Kubectl::CLIWrapper::Result {
-  use Moose;
-  has rc => (is => 'ro', isa => 'Int', required => 1);
-  has output => (is => 'ro', isa => 'Str');
-  has json => (is => 'ro', isa => 'HashRef');
-
-  has success => (is => 'ro', isa => 'Bool', lazy => 1, default => sub {
-    my $self = shift;
-    $self->rc == 0;
-  });
-}
 package Kubectl::CLIWrapper {
   use Moose;
   use JSON::MaybeXS;
   use IPC::Open3;
+  use Kubectl::CLIWrapper::Result;
 
   has server => (is => 'ro', isa => 'Str');
   has username => (is => 'ro', isa => 'Str');
