1032f21db82e3e027dacdc11ac573a222fc72e78
Jose Luis Martinez <jlmartinez@capside.com>
2018-05-06 01:09:51 +0200

    Packaging and documentation

--- lib/Kubectl/CLIWrapper.pm
+++ lib/Kubectl/CLIWrapper.pm
@@ -4,6 +4,8 @@ package Kubectl::CLIWrapper {
   use IPC::Open3;
   use Kubectl::CLIWrapper::Result;
 
+  our $VERSION = '0.01';
+
   has server => (is => 'ro', isa => 'Str');
   has username => (is => 'ro', isa => 'Str');
   has password => (is => 'ro', isa => 'Str');
@@ -86,3 +88,127 @@ package Kubectl::CLIWrapper {
 
 }
 1;
+### main pod documentation begin ###
+
+=encoding UTF-8
+
+=head1 NAME
+
+Kubectl::CLIWrapper - Module to use the Kubernetes API via the kubectl CLI
+
+=head1 SYNOPSIS
+
+  use Kubectl::CLIWrapper;
+
+  my $kube = Kubectl::CLIWrapper->new(
+    server => 'https://kubernetes.example.org/',
+    username => 'user',
+    password => 'pass',
+  );
+
+  my $result = $kube->run('explain', 'service');
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command
+
+  my $result = $kube->json('get', 'pods');
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command
+  # $result->json is a hashref with the result of the parsed JSON output of the command
+
+  my $result = $kube->input('{"kind":"Service" ... }', 'create', '-f', '-');
+  # $result->success == 1 if the command executed correctly
+  # $result->output contains the output of the command 
+
+=head1 DESCRIPTION
+
+This module helps you use the Kubernetes API. It sends all it's commands
+via the CLI command line tool C<kubectl>. You can find kubectl installation instructions
+here L<https://kubernetes.io/docs/tasks/tools/install-kubectl/>.
+
+=head1 CREDENTIALS
+
+Kubectl::CLIWrapper attributes are mainly the options you can pass C<kubectl> to control
+how it authenticates to the Kubernetes server. Run C<kubectl options> to discover what these
+options do. If you don't initialize any attributes, kubectl will behave just like on the command 
+line (loading ~/.kube/config) which may be already set to point to a Kubernetes server
+
+=head1 ATTRIBUTES
+
+=head2 kubectl
+
+By default initialized to C<kubectl>. It will try to find kubectl in the PATH. You can
+set it explicitly to specific kubectl excecutable.
+
+=head2 server
+
+The URL of the Kubernetes service
+
+=head2 username
+
+The username for Basic Authentication
+
+=head2 password
+
+The password for Basic Authentication
+
+=head2 token
+
+The Bearer token for authentication
+
+=head2 insecure_tls
+
+A Boolean flag that tells kubectl to not verify the certificate of the server it connects to
+
+=head2 namespace
+
+The Kubernetes namespace to operate in. Defaults to C<default>.
+
+=head1 METHODS
+
+=head2 run(@parameters)
+
+Will run kubectl with the parameters. Returns a L<Kubectl::CLIWrapper::Result> object
+with C<output> set to the output of the command, and C<success> a Boolean to indicate
+if the command reported successful execution.
+
+=head2 json(@parameters)
+
+Will run kubectl with the parameters, and C<'-o=json'>. Returns a L<Kubectl::CLIWrapper::Result> object
+with C<output> set to the output of the command, and C<json> set to a hashref with the parsed
+JSON. C<success> will be false if JSON parsing fails.
+
+=head2 input($input_to_kubectl, @parameters)
+
+Will run kubectl with the parametes, sending $input_to_kubectl on it's STDIN. 
+Returns a L<Kubectl::CLIWrapper::Result> object with C<output> set to the output of the command. 
+C<success> will be set accordingly.
+
+=head1 SEE ALSO
+
+L<https://kubernetes.io/docs/tasks/tools/install-kubectl/>
+
+L<K8s::IO>
+
+L<https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.10/>
+
+L<Net::Kubernetes>
+
+=head1 AUTHOR
+
+    Jose Luis Martinez
+    CAPSiDE
+    jlmartinez@capside.com
+
+=head1 BUGS and SOURCE
+
+The source code is located here: L<https://github.com/pplu/kubectl-cliwrapper>
+
+Please report bugs to: L<https://github.com/pplu/kubectl-cliwrapper/issues>
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2018 by CAPSiDE
+This code is distributed under the Apache 2 License. The full text of the 
+license can be found in the LICENSE file included with this module.
+
+=cut
